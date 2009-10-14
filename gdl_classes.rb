caf2e0c594ee54ce01571c6137fd863a3fdb8ce5
Ulysses Bonfim <ulyssesb@gmail.com>
2009-10-14 00:28:45 -0300

    Array_To_Parser done (falta testar direito)

--- gdl_classes.rb
+++ gdl_classes.rb
@@ -3,10 +3,12 @@ class Term
   attr_accessor :name, :params
 
   def initialize(name, params=[])
-    if not name.is_a? Symbol
+    if name.is_a? String
       @name = name.index("?") == 0 ? name.gsub(/^\?/,'').upcase.to_sym : name.to_sym
-    else
+    elsif not name.nil? 
       @name = name
+    else 
+      return nil
     end
     @params = params.nil?? [] : params
   end
@@ -16,7 +18,7 @@ class Term
   end
 
   def is_atom?
-    (@params.empty?) ? true : false
+    @params.empty?
   end
 
   def is_var?
@@ -41,11 +43,63 @@ class Term
     string
   end
 
+  # Procura nos parametros todas as variáveis. Retorna um array
+  def vars
+    vars_list = @params.collect do |param|
+      if param.is_var?
+        param.name
+      elsif not param.is_atom?
+        param.vars
+      end
+    end
+    vars_list.flatten.compact
+  end
+
+  # <- Hash na forma :var => [val1, val2]
+  # -> Array com os termos gerados a partir da valoração
+  def bind_vars(val_hash)
+    # Cria um termo para cada valoração possível
+    if self.is_var? and val_hash.has_key? @name
+      return val_hash[@name].collect { |val| Term.new val}
+
+    # Copia a si mesmo n vezes, onde n é o número de valorações possíveis
+    elsif self.is_atom?
+      n = val_hash.first.at(1).length rescue nil
+      dumps = []
+      n.times { dumps << self.dup } unless n.nil?
+      dumps
+    else 
+      # Coleta as valorações
+      binded = @params.collect { |param| param.bind_vars(val_hash) }
+      binded = binded.transpose # Transforma em um array [[valA1, valB1...]]
+      # Retorna um array com todas as valorações
+      return binded.collect { |params| Term.new(@name, params)}
+    end
+  end
+
   # Recebe um string na forma [[foo,bar], [rab, oof]], onde cada tupla do 
   # vetor é uma possivel valoração para as variáves do termo
   # Retorna os termos instanciados com as _n_ valorações possíveis
-  def arr_val_parser(val_array)
-    
+  def val_array_parser(val_array)
+    vars = @params.vars
+
+    # Verifica se as variáveis estão em mesmo número que as valorações
+    return nil if vars.size != val_array.size
+  
+    # Transforma o array 
+    # [[valA1, valB1], [valA2, valB2]] em 
+    # [[valA1, valA2], [valB1, valB2]] 
+    val_array = val_array.transpose
+
+    # Faz a união dos arrays em duplas [var, vals]
+    tuples = vars.zip val_array
+
+    # Transforma em uma hash :var => val
+    val_hash = {}
+    tuples.each { |var, val| val_hash.store var, val }
+
+    # Cria os novos termos
+    return self.bind_vars(val_hash)
   end
 end
 
